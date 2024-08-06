82f935628835761c2ce519faef3fe2c493cb700b
unknown <leepoewun@gmail.com>
2024-08-06 23:06:33 +0800

    added dedicated sql pool scripts

---
+++ 02_synapse/02_02_dedicated_sql.sql
@@ -0,0 +1,66 @@
+--loading external table data into dedicated sql pool table
+--initialising script in 2a
+
+--[POLYBASE]--
+--- note that no additional SAS token required 
+---as polybase will use underlying credentials made on activityLog
+CREATE TABLE PoolActivityLog
+WITH (
+    DISTRIBUTION = ROUND_ROBIN
+)
+AS
+SELECT * FROM activityLog;
+
+
+select * from PoolActivityLog;
+
+--[COPY INTO command]--
+
+drop table PoolActivityLog;
+---step 1: to have table created first
+CREATE TABLE PoolActivityLog(
+    [Correlationid] varchar(200),
+    [Operationname]	varchar(300),
+    [Status]	varchar(100),
+    [Eventcategory]	varchar(100),
+    [Level]	varchar(100),
+    [Time]	varchar(100),
+    [Subscription]	varchar(200),
+    [Eventinitiatedby]	varchar(1000),
+    [Resourcetype]	varchar(300),
+    [Resourcegroup]	varchar(1000),
+    [Resource] varchar(2000)
+)
+WITH(
+    DISTRIBUTION=ROUND_ROBIN
+);
+
+---step 2: Copy into, with credentials required to specify
+------CSV------
+COPY INTO PoolActivityLog
+FROM 'https://storageforexampoe.blob.core.windows.net/data/ActivityLog-01.csv'
+WITH(
+    FILE_TYPE='CSV',
+    FIRST_ROW=2,
+    CREDENTIALS=(
+        IDENTITY='Shared Access Signature',
+        SECRET='sv=2022-11-02&ss=bf&srt=sco&sp=rlx&se=2025-08-06T18:27:47Z&st=2024-08-06T10:27:47Z&spr=https&sig=cKnovoTT3l68sDsTHiHmp7znOO4o6PNOSz0oUE4oaf8%3D'
+    )
+);
+
+------Parquet------
+COPY INTO PoolActivityLog
+FROM 'https://storageforexampoe.blob.core.windows.net/data/ActivityLog-01.parquet'
+WITH(
+    FILE_TYPE='PARQUET',
+    CREDENTIAL=(
+        IDENTITY='Shared Access Signature',
+        SECRET='sv=2022-11-02&ss=bf&srt=sco&sp=rlx&se=2025-08-06T18:27:47Z&st=2024-08-06T10:27:47Z&spr=https&sig=cKnovoTT3l68sDsTHiHmp7znOO4o6PNOSz0oUE4oaf8%3D'
+    )
+);
+
+select * from PoolActivityLog;
+
+--[PIPELINES]--
+DELETE from PoolActivityLog;
+--see integrate tab
---
+++ 02_synapse/02_02a_initialise_dedicated_pool_polybase.sql
@@ -0,0 +1,93 @@
+--[Configure data source]--
+---encryption key for secrets
+CREATE MASTER KEY ENCRYPTION BY PASSWORD='P@ssw@rd123';
+
+
+---scoped credential - this is where SAS token will sit
+CREATE DATABASE SCOPED CREDENTIAL storageAccessCredSAS
+WITH IDENTITY = 'SHARED ACCESS SIGNATURE',
+SECRET = 'sv=2022-11-02&ss=bf&srt=sco&sp=rlx&se=2025-08-06T18:27:47Z&st=2024-08-06T10:27:47Z&spr=https&sig=cKnovoTT3l68sDsTHiHmp7znOO4o6PNOSz0oUE4oaf8%3D';
+
+
+---finally create the external data source with scoped credential referencing above created
+CREATE EXTERNAL DATA SOURCE srcActivity
+WITH (
+    LOCATION='https://storageforexampoe.blob.core.windows.net/data',
+    CREDENTIAL=storageAccessCredSAS
+
+);
+
+--[Configure file format]--
+
+---e.g.1 Parquet
+CREATE EXTERNAL FILE FORMAT parquetFileFormat
+WITH(
+    FORMAT_TYPE= PARQUET,
+    DATA_COMPRESSION='org.apache.hadoop.io.compress.SnappyCodec'
+);
+---e.g.2 csv
+CREATE EXTERNAL FILE FORMAT csvFileFormat
+WITH(
+    FORMAT_TYPE= DELIMITEDTEXT,
+    FORMAT_OPTIONS(
+        FIELD_TERMINATOR=',',
+        FIRST_ROW = 2
+    )
+);
+---e.g.3 JSON
+CREATE EXTERNAL FILE FORMAT jsonFileFormat
+WITH(
+    FORMAT_TYPE= JSON,
+    DATA_COMPRESSION='org.apache.hadoop.io.compress.SnappyCodec'
+);
+
+--[configure external table def]
+CREATE EXTERNAL TABLE activityLog(
+    [Correlationid] varchar(200),
+    [Operationname]	varchar(300),
+    [Status]	varchar(100),
+    [Eventcategory]	varchar(100),
+    [Level]	varchar(100),
+    [Time]	varchar(100),
+    [Subscription]	varchar(200),
+    [Eventinitiatedby]	varchar(1000),
+    [Resourcetype]	varchar(300),
+    [Resourcegroup]	varchar(1000),
+    [Resource] varchar(2000)
+)
+WITH(
+    LOCATION='/*.parquet',
+    DATA_SOURCE=srcActivity,
+    FILE_FORMAT=parquetFileFormat
+);
+
+
+select * from activityLog;
+--JSON use OPENROWSET
+--prerequisite : current login user requires Storage Blob Data Read access
+select 
+    JSON_VALUE(jsonContent, '$.Correlationid') as [Correlationid],
+    JSON_VALUE(jsonContent, '$.Operationname') as [Operationname],
+    JSON_VALUE(jsonContent, '$.Eventcategory') as [Eventcategory],
+    JSON_VALUE(jsonContent, '$.Level') as [Level],
+    JSON_VALUE(jsonContent, '$.Time') as [Time],
+    JSON_VALUE(jsonContent, '$.Subscription') as [Subscription],
+    JSON_VALUE(jsonContent, '$.Eventinitiatedby') as [Eventinitiatedby],
+    JSON_VALUE(jsonContent, '$.Resourcetype') as [Resourcetype],
+    JSON_VALUE(jsonContent, '$.Resourcegroup') as [Resourcegroup],
+    JSON_VALUE(jsonContent, '$.Resource') as [Resource]
+
+from OPENROWSET(
+    bulk 'https://storageforexampoe.blob.core.windows.net/data/ActivityLog-01.json',
+    format = 'csv',
+    fieldterminator ='0x0b',
+    fieldquote = '0x0b',
+    rowterminator='0x0a'
+)with (jsonContent varchar(MAX) ) as rows
+
+
+--Test on quering external data
+
+select count(*) from activityLog;
+
+--
